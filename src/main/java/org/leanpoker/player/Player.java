349c714498f21c246c62d5f6dc6babab93637ef3
Peter Borsos <borsospeterlaszlo@gmail.com>
2019-05-20 11:20:35 +0200

    Log try.

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -8,7 +8,10 @@ public class Player {
 
     static final String VERSION = "1.0";
 
-    public static int betRequest(JsonElement request) { return 0;}
+    public static int betRequest(JsonElement request) {
+        System.err.println("próba");
+        return 0;
+    }
 
     public static void showdown(JsonElement game) {
     }
