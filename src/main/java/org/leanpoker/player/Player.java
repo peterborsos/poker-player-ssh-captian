b6ee0e654ee51f74d2d01e70f556494ee721fc08
Peto Reka <peto.reka0@gmail.com>
2019-05-20 11:31:48 +0200

    Deletet PlayerInformation class

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,37 +1,17 @@
 package org.leanpoker.player;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
-
-import java.util.Map;
 
 public class Player {
 
     static final String VERSION = "1.2";
 
     public static int betRequest(JsonElement request) {
-        GsonBuilder builder = new GsonBuilder();
-        builder.setPrettyPrinting();
-        Gson gson = builder.create();
-        PlayerInformation playerInformation = gson.fromJson(request, PlayerInformation.class);
-        System.err.println(playerInformation.name);
-        //System.err.println(request.getAsJsonObject().get("players"));
+        System.err.println(request.getAsJsonObject().get("players"));
         return 0;
     }
 
     public static void showdown(JsonElement game) {
     }
 
-}
-class PlayerInformation {
-    public String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
