7d0e4df2b5e00cb06d517d966788d29d5b32249a
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-05-20 11:26:07 +0200

    Get players.

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -9,7 +9,7 @@ public class Player {
     static final String VERSION = "1.0";
 
     public static int betRequest(JsonElement request) {
-        System.err.println("próba SSH captain");
+        System.err.println(request.getAsJsonObject().get("players"));
         return 0;
     }
 
