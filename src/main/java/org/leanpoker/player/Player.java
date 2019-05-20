ff60addf9563b65882d27832fbde0b16d1dff3cd
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-05-20 11:53:01 +0200

    log player objects

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -3,9 +3,6 @@ package org.leanpoker.player;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Player {
 
     static final String VERSION = "1.2";
@@ -13,11 +10,10 @@ public class Player {
     public static int betRequest(JsonElement request) {
         //System.err.println(request.getAsJsonObject().get("players"));
         JsonArray players = request.getAsJsonObject().get("players").getAsJsonArray();
-        for (Object player : players) {
+        for (JsonElement player : players) {
+            player.getAsJsonObject();
             System.err.println(player);
         }
-
-
         return 0;
     }
 
