5dd5dfedf05d46b50ef21fdbb700a7ad1526b7e4
Peter Borsos <borsospeterlaszlo@gmail.com>
2019-05-20 10:43:47 +0200

    Back to default.

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -6,11 +6,9 @@ import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "SSH Captian";
+    static final String VERSION = "Default Java folding player";
 
-    public static int betRequest(JsonElement request) {
-        request.getAsJsonArray();
-        return 5;}
+    public static int betRequest(JsonElement request) { return 0;}
 
     public static void showdown(JsonElement game) {
     }
--- src/main/java/org/leanpoker/player/PlayerServlet.java
+++ src/main/java/org/leanpoker/player/PlayerServlet.java
@@ -14,7 +14,7 @@ public class PlayerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().print("SSH Captain is running");
+        resp.getWriter().print("Java player is running");
     }
 
     @Override
