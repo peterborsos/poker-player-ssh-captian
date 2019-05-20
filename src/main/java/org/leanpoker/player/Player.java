adfe380ab2e54c73e6202aacc81698a63acc10be
Peto Reka <peto.reka0@gmail.com>
2019-05-20 11:28:27 +0200

    Added PalyerInformation class

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,18 +1,36 @@
 package org.leanpoker.player;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 
 import java.util.Map;
 
 public class Player {
 
-    static final String VERSION = "1.0";
+    static final String VERSION = "1.2";
 
     public static int betRequest(JsonElement request) {
-        System.err.println("próba SSH captain");
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        Gson gson = builder.create();
+        PlayerInformation playerInformation = gson.fromJson(request, PlayerInformation.class);
+        System.err.println(playerInformation.name);
         return 0;
     }
 
     public static void showdown(JsonElement game) {
+    }
+
+}
+class PlayerInformation {
+    public String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
