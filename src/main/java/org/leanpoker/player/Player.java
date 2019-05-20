68f67f1fe982c8c2b5db43e4f588d40b77991514
ZsuzsaBenko <benkozsuzsa11@gmail.com>
2019-05-20 12:00:15 +0200

    bugfix

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -5,7 +5,7 @@ import com.google.gson.JsonElement;
 
 public class Player {
 
-    static final String VERSION = "1.2";
+    static final String VERSION = "1.3";
 
     public static int betRequest(JsonElement request) {
         //System.err.println(request.getAsJsonObject().get("players"));
